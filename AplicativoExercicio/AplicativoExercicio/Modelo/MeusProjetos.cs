2a8eaad55e12220904d8aabf10f366f6f2434b8e
raquel anjos <raquel.santosanjos@hotmail.com>
2020-06-30 12:47:09 -0300

    Criacao de listaviews

---
+++ AplicativoExercicio/AplicativoExercicio/Modelo/MeusProjetos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AplicativoExercicio.Modelo
+{
+    public class MeusProjetos
+    {
+        public string Nome { get; set; }
+        public string Curso { get; set; }
+        public string Docente { get; set; }
+        public string Situacao { get; set; }
+        
+        public string Finzalizacao { get; set; }
+        public string Descricao { get; set; }
+        public string Integrantes { get; set; }
+    }
+}
---
+++ AplicativoExercicio/AplicativoExercicio/Views/MeusProjetosPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using AplicativoExercicio.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace AplicativoExercicio.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MeusProjetosPage : ContentPage
+    {
+        private ObservableCollection<MeusProjetos> projetos = new ObservableCollection<MeusProjetos>();
+        public MeusProjetosPage()
+        {
+            InitializeComponent();
+
+            ListaInformacao.ItemsSource = projetos;
+
+            projetos.Add(new MeusProjetos
+            {
+                Nome = "Desenvolvimento de Sistema e Aplicativo",
+                Curso = "Informatica ",
+                Docente = "Carlos e Wellington",
+                Descricao = "Aprender a Desenvolver sistemas e aplicativos",
+                Integrantes = "23"
+            });
+        }
+    }
+}
